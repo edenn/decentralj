d232ae809339708cb0541394961b73753fd03f96
uwec <uwe650@hotmail.com>
2014-08-23 21:07:46 -0400

    Adding User

---
+++ src/main/java/com/decentralbank/decentralj/net/user/User.java
@@ -0,0 +1,65 @@
+package com.decentralbank.decentralj.net.user;
+
+import java.io.Serializable;
+import java.security.KeyPair;
+import java.util.List;
+
+import com.decentralbank.decentralj.core.NodeWallet;
+
+// The User is persisted locally
+// The User Class represents the person who sets up the server.
+
+public class User implements Serializable {
+	
+	//create transient property for states
+	
+	private KeyPair messageKeyPair;
+	private String accountID;
+	// TODO make it thread safe
+	private List<NodeWallet> Accounts;
+	private NodeWallet currentAccount;
+	
+	public void addBankAccount(NodeWallet Account){
+	        
+		   if (!Accounts.contains(Account)){
+			 
+			   Accounts.add(Account);
+		   
+		   }
+
+	       setCurrentAccount(Account);
+	}
+
+	public String getAccount(){
+		return accountID;
+		   
+	}
+	
+	private void setCurrentAccount(NodeWallet account) {
+	
+		this.currentAccount = account;	   
+		
+	}
+
+	public void removeCurrentAccount(){
+		
+	        if (currentAccount != null){
+	        	
+	        	Accounts.remove(currentAccount);
+	       
+	        }
+
+	        if (Accounts.isEmpty()){
+	        	
+	        	currentAccount = null;
+	        
+	        } else{
+	        	
+	        	setCurrentAccount(Accounts.get(0));
+	       
+	        }
+	}
+	    
+
+
+}
