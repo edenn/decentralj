f5d97006a138166d1a80abd190bcdc7cb4f65834
leo <lshao30@gmail.com>
2014-08-30 18:33:59 -0400

    RequestTest
    made ze tests for request and changed constructors

--- src/main/java/com/decentralbank/decentralj/core/Request.java
+++ src/main/java/com/decentralbank/decentralj/core/Request.java
@@ -8,67 +8,44 @@ public class Request {
 	private String message;
 	
 	public Request() {
-		
+		this("","","");
 	}
 	
 	public Request(String status, String peerID) {
-		
-		this.status = status;
-		this.peerID = peerID;
-		this.message = "";
-		
+		this(status, peerID, "");
 	}	
 	public Request(String status, String peerID, String responseMessage){
-		
 		this.status = status;
 		this.peerID = peerID;
 		this.message = responseMessage;
-		
 	}
 	
 	public String getStatus() {
-		
 		return status;
-		
 	}
 	
 	public void setStatus(String status) {
-		
 		this.status = status;
-		
 	}
 	
 	public String getPeerID() {
-		
 		return peerID;
-		
 	}
 
 	public void setPeerID(String peerID) {
-		
 		this.peerID = peerID;
-	
 	}
 
 	public String getMessage() {
-	
 		return message;
-	
 	}
 	
 	public void setMessage(String message) {
-	
 		this.message = message;
-	
 	}
 	
-	public String toString()
-	{
-
-		return this.getStatus()+" "+this.getPeerID()+" "+this.getMessage();
-	
+	public String toString() {
+		return this.getStatus()+" "+this.getPeerID()+" "+this.getMessage();	
 	}
-	
-	
 
 }
