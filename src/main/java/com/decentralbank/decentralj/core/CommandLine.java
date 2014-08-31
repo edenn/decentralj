83dacd957662e90d4141aa21899af538605100a8
leo <lshao30@gmail.com>
2014-08-30 20:00:38 -0400

    command line proccessing examples

--- src/main/java/com/decentralbank/decentralj/core/CommandLine.java
+++ src/main/java/com/decentralbank/decentralj/core/CommandLine.java
@@ -1,12 +1,63 @@
 package com.decentralbank.decentralj.core;
 
-public class CommandLine {
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CommandLine {	
 	
-	public void command1(){
+	static HashMap<String, ICommand> map = new HashMap<>();
 	
+	public static void main(String args[]) {
+		map.put("hello", new printHello());
+		map.put("string", new printString());
+		map.put("add", new add());
+		map.get("hello").execute();
+		map.get("string").execute("i am here");
+		map.get("add").execute(1,2);
+	}
+}
+
+interface ICommand {
+	public void execute(Object ... args);
+}
+
+class printHello implements ICommand
+{
+	@Override
+	public void execute(Object ... args) {
+		System.out.println("Hello");
+	}	
+}
+
+class printString implements ICommand
+{
+	@Override
+	public void execute(Object... args) {
+		String out = "";
+		for(Object s : args)
+			out += s;
+		System.out.println(out);
+	}
+}
+
+class add implements ICommand
+{
+
+	@Override
+	public void execute(Object... args) {
+		try {
+			if(args.length == 2)
+			{
+				int a = (int)args[0];
+				int b = (int)args[1];
+				System.out.println(a + b);
+			}
+			else throw new Exception("incorrect num of inputs");
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
 		
 	}
 	
-	
-
 }
