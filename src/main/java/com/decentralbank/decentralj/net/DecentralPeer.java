3c485286c338a7ff75dadf211987967589cdc512
uwec <uwe650@hotmail.com>
2014-10-04 16:13:21 -0400

    adding new properties to DecentralPeer

--- src/main/java/com/decentralbank/decentralj/net/DecentralPeer.java
+++ src/main/java/com/decentralbank/decentralj/net/DecentralPeer.java
@@ -26,7 +26,7 @@ public class DecentralPeer {
 	private transient long    lastCheckTime = 0;
 	
 	private ZContext ctx;                //  CZMQ context
-    private Socket mailbox;              //  Socket through to peer
+    private Socket socket;              //  Socket through to peer
     private String identity;             //  Identity string
     private String endpoint;             //  Endpoint connected to
     private long evasive_at;             //  Peer is being evasive
@@ -60,27 +60,27 @@ public class DecentralPeer {
     }
     
     //  ---------------------------------------------------------------------
-    //  Connect peer mailbox
-    //  Configures mailbox and connects to peer's router endpoint
+    //  Connect peer Decentral
+    //  Configures socket and connects to peer's router endpoint
     public void connect (String replyTo, String endpoint)
     {
-        //  Create new outgoing socket (drop any messages in transit)
-        mailbox = ctx.createSocket (ZMQ.DEALER);
+        //  Create a Dealer socket (drop any messages in transit)
+        socket = ctx.createSocket (ZMQ.DEALER);
 
         //  Null if shutting down
-        if (mailbox != null) {
+        if (socket != null) {
             //  Set our caller 'From' identity so that receiving node knows
             //  who each message came from.
-            mailbox.setIdentity (replyTo.getBytes ());
+            socket.setIdentity (replyTo.getBytes ());
     
             //  Set a high-water mark that allows for reasonable activity
-            mailbox.setSndHWM (ZreInterface.PEER_EXPIRED * 100);
+            socket.setSndHWM (ZreInterface.PEER_EXPIRED * 100);
            
             //  Send messages immediately or return EAGAIN
-            mailbox.setSendTimeOut (0);
+            socket.setSendTimeOut (0);
     
             //  Connect through to peer node
-            mailbox.connect (String.format ("tcp://%s", endpoint));
+            socket.connect (String.format ("tcp://%s", endpoint));
             this.endpoint = endpoint;
             connected = true;
             ready = false;        
@@ -107,7 +107,7 @@ public class DecentralPeer {
             if (++sent_sequence > USHORT_MAX)
                 sent_sequence = 0;
             msg.setSequence (sent_sequence);
-            if (!msg.send (mailbox)) {
+            if (!msg.send (socket)) {
                 disconnect ();
                 return false;
             }
@@ -128,7 +128,7 @@ public class DecentralPeer {
 //  No more messages will be sent to peer until connected again
     public void disconnect ()
     {
-        ctx.destroySocket (mailbox);
+        ctx.destroySocket(socket);
         endpoint = null;
         connected = false;
     }
@@ -222,12 +222,56 @@ public class DecentralPeer {
 	        return valid;
 	    }
 
+	    //  ---------------------------------------------------------------------
+	    //  Return peer connection endpoint
+	    public String endpoint ()
+	    {
+	        if (connected)
+	            return endpoint;
+	        else
+	            return "";
+	                
+	    }
 
 		public void invoke(ObjectName timer, String string, Object[] objects,
 				String[] strings) {
 			// TODO Auto-generated method stub
 			
 		}
+		
+		//  Return peer future expired time
+	    public long expiredAt ()
+	    {
+	        return expired_at;
+	    }
+
+	    //  ---------------------------------------------------------------------
+	    //  Return peer future evasive time
+	    public long evasiveAt ()
+	    {
+	        return evasive_at;
+	    }
+
+	    
+	    public void setReady (boolean ready)
+	    {
+	        this.ready = ready;
+	    }
+
+	    public boolean ready ()
+	    {
+	        return ready;
+	    }
+
+	    public void setStatus (int status)
+	    {
+	        this.status = status;
+	    }
+	    
+	    public int status ()
+	    {
+	        return status;
+	    }
 
 
 
