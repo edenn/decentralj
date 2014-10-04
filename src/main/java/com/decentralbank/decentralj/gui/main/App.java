28ed8950f50010b98065b7024d754b932d2cc2eb
uwec <uwe650@hotmail.com>
2014-10-03 20:41:07 -0400

    adding font-awesome icons.

--- src/main/java/com/decentralbank/decentralj/gui/main/App.java
+++ src/main/java/com/decentralbank/decentralj/gui/main/App.java
@@ -6,22 +6,35 @@
 package com.decentralbank.decentralj.gui.main;
 
 import java.io.IOException;
+
+import de.jensd.fx.fontawesome.AwesomeDude;
+import de.jensd.fx.fontawesome.AwesomeIcon;
+import de.jensd.fx.fontawesome.AwesomeIconsStack;
+import de.jensd.fx.fontawesome.Icon;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class App extends Application {
+	static {
+        Font.loadFont(App.class.getResource("fontawesome-webfont.ttf").toExternalForm(), 10);
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
 
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-
+        Parent root = FXMLLoader.load(getClass().getResource("Main2.fxml"));
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
+        stage.setTitle("Decentral Bank");
         stage.show();
     }
 
